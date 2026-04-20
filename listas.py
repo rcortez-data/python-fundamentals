bee096acc49bd63e9d2c7adf27aba9e2749fc07f
rcortez-data <ricardocd051@gmail.com>
2026-04-19 18:05:01 -0600

    hice un ejercicio de lista y que imprima solo las que tengan 4 letras o mas en su nombre

--- listas.py
+++ listas.py
@@ -2,3 +2,13 @@
 nombres = ["ana", "luis", "maría"]
 for nombre in nombres:
     print(nombre.upper())
+
+
+# Ejercicio 1
+
+frutas = ["manzana", "naranja", "uva", "pera", "mango"]
+
+for fruta in frutas:
+    if len(fruta) >= 4:
+        print(fruta)
+        
