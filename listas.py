5c8bebbb75cb1bd6b1c0cf0e7dce501747577700
rcortez-data <ricardocd051@gmail.com>
2026-04-19 22:00:41 -0600

    realize ejercicio #2

--- listas.py
+++ listas.py
@@ -9,6 +9,19 @@ for nombre in nombres:
 frutas = ["manzana", "naranja", "uva", "pera", "mango"]
 
 for fruta in frutas:
-    if len(fruta) >= 4:
-        print(fruta)
-        
+    if len(fruta) > 4:
+        print(fruta.upper())
+
+
+
+# Ejercicio 2
+
+numeros = [4, 7, 2, 19, 5, 11, 3, 8]
+cuenta = 0
+
+for numero in numeros:
+    if numero > 6:
+        cuenta += 1
+        print(numero)
+print(cuenta)
+
