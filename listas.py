375720a3d4efbd94c34f1c4c13adbecce1cc64fe
rcortez-data <ricardocd051@gmail.com>
2026-04-21 17:36:30 -0600

    Realize ejercicio #3

--- listas.py
+++ listas.py
@@ -25,3 +25,22 @@ for numero in numeros:
         print(numero)
 print(cuenta)
 
+
+# # Ejercicio 3: FizzBuzz
+# Instrucciones:
+# Escribe un programa que imprima los números del 1 al 50, pero con estas reglas:
+
+# Si el número es divisible entre 3, imprime "Fizz" en lugar del número.
+# Si el número es divisible entre 5, imprime "Buzz" en lugar del número.
+# Si el número es divisible entre 3 y 5 al mismo tiempo, imprime "FizzBuzz".
+# Si no cumple ninguna condición, imprime el número normal.
+
+for i in range (1,51):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
