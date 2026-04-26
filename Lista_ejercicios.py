101f72e5b19da24007efca9e54c3fb90f2c869a2
rcortez-data <ricardocd051@gmail.com>
2026-04-25 19:12:55 -0600

    Realize ejercicio #4

--- Lista_ejercicios.py
+++ Lista_ejercicios.py
@@ -69,4 +69,13 @@ while True:
         break
 
 
+# ejercicio #4
+
+nombre = input("Escribe tu nombre:")
+puesto = input("Escribe tu puesto:")
+salario = float(input("Escribe tu salario:"))
+informacion = {"Nombre":nombre, "Puesto":puesto, "Salario":salario}
+print("Nombre: "+informacion["Nombre"])
+print("Puesto: "+informacion["Puesto"])
+print(f"Salario: ${informacion['Salario']:,.2f}")
 
--- ejercicios.py
+++
@@ -1,7 +0,0 @@
-nombre = input("Escribe tu nombre:")
-puesto = input("Escribe tu puesto:")
-salario = float(input("Escribe tu salario:"))
-informacion = {"Nombre":nombre, "Puesto":puesto, "Salario":salario}
-print("Nombre: "+informacion["Nombre"])
-print("Puesto: "+informacion["Puesto"])
-print(f"Salario: ${informacion['Salario']:,.2f}")
