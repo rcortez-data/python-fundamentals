e4cb360718b53e5c7f71be2f19be0d2d29f4293e
rcortez-data <ricardocd051@gmail.com>
2026-05-04 00:02:54 -0600

    Realize ejercicio #9

--- ejercicios.py
+++ ejercicios.py
@@ -1,9 +1,16 @@
-for i in range(1, int(input("Dime un numero: "))+1):
-
-    num = i
-
-    if num % 2 == 0:
-        paridad = "par"
+def clasificar_numero(n):
+    if n > 0:
+        tipo = "Positivo"
+    elif n < 0:
+        tipo = "Negativo"
     else:
-        paridad = "impar"
-    print(f"{i} {paridad}")
+        tipo = "Cero"
+
+    if n != 0:
+        paridad = "par" if n % 2 == 0 else "impar"
+        print(f"{tipo} {paridad}")
+    else:
+        print(tipo)
+
+numero = int(input("Escribe un número entero: "))
+clasificar_numero(numero)
