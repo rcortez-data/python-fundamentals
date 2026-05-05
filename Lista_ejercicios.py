187257932e1a126d477b1904a466760e26df3c40
rcortez-data <ricardocd051@gmail.com>
2026-05-05 01:46:17 -0600

    Realize ejercicio #11

--- Lista_ejercicios.py
+++ Lista_ejercicios.py
@@ -186,3 +186,18 @@ def numero(n):
         print(f'{n} x {i} = {n*i}')
 n = int(input("Ingresa un número: "))
 numero(n)
+
+# Ejercicio #11
+
+def numero(n):
+    for i in range(1, n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+n = int(input("Ingresa un número: "))
+numero(n)
--- ejercicios.py
+++ ejercicios.py
@@ -1,5 +1,12 @@
 def numero(n):
-    for i in range(1,11):
-        print(f'{n} x {i} = {n*i}')
+    for i in range(1, n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
 n = int(input("Ingresa un número: "))
 numero(n)
